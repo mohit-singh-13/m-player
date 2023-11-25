3c28d6c239f450675b0f9871b734fbcfd085ef92
Mohit Singh <140334981+mohit-singh-13@users.noreply.github.com>
2023-11-26 01:09:30 +0530

    Update connection.js

--- Backend/database/connection.js
+++ Backend/database/connection.js
@@ -1,5 +1,4 @@
-// const URL = 'mongodb+srv://mohit_singh_13:hellomohit123@cluster0.rrtthhn.mongodb.net/?retryWrites=true&w=majority';
-const URL = 'mongodb+srv://mohit:mohit123@cluster0.adj712w.mongodb.net/?retryWrites=true&w=majority';
+// database link here
 import mongoose from "mongoose";
 
 const promise = mongoose.connect(URL);
