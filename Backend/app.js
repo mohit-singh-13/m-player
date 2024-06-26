08bb0f04f42d014bf0bdaf612c48dfb0958c501b
mohit-singh-13 <mohitsingh092003@gmail.com>
2024-06-26 16:29:24 +0530

    Add origin

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Your frontend URL
+    origin: 'https://ms-m-player.vercel.app/', // Your frontend URL
     methods: ['GET', 'POST'],
     credentials: true, // Allow credentials (cookies) to be sent
     allowedHeaders: ['Content-Type', 'Authorization'],
